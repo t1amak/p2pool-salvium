3a6d0fea73cc86cddc590b6adc10a60773f8f81d
t1amak <tiamaczek@gmail.com>
2025-11-13 17:51:14 +0000

    docker fix

--- scripts/release/linux_x64/build.sh
+++ scripts/release/linux_x64/build.sh
@@ -4,13 +4,15 @@
 
 cd "$(dirname "$0")"
 
+repo_root="$(cd ../../.. && pwd)"
+
 if [ "$2" ]; then
 	cpu_set="--cpuset-cpus $2"
 else
 	cpu_set=""
 fi
 
-docker build $cpu_set --build-arg P2POOL_VERSION=$1 -t p2pool_linux_x64_build_$1 .
+docker build $cpu_set --build-arg P2POOL_VERSION=$1 -t p2pool_linux_x64_build_$1 -f Dockerfile "$repo_root"
 
 docker create --name p2pool_linux_x64_build_$1_container p2pool_linux_x64_build_$1:latest
 docker cp p2pool_linux_x64_build_$1_container:/p2pool/build/p2pool-$1-linux-x64.tar.gz ../p2pool-$1-linux-x64.tar.gz
